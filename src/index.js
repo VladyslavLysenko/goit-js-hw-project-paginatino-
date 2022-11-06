c76a0eb02187aabfb2d5f23c53d2358cfdaa2c25
VladyslavLysenko <Dodog12362@gmail.com>
2022-11-06 09:58:53 +0200

    hi

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './sass/index.scss';
+
+
+
+import './js/collection';
+import './js/modal-footer';
+
+import './js/addFilmsAndrenderGalery'
+
+
+import './js/service';
+
+import './js/markups/filmDetailMarkup';
+
+import './js/openFilmModal'
+import './js/toggleTheme';
